2474fdf9731d58910b3fe3c0cc0afbdee6892ae2
Guilherme Juventino <guilhermejuventino@gmail.com>
2023-10-02 15:28:45 -0300

    Added animation for the player

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 180.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 130.0
+const JUMP_VELOCITY = -200.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animation: AnimatedSprite2D = $Animation
 
 
 func _physics_process(delta):
@@ -24,9 +26,23 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	animate_player()
 
 	move_and_slide()
 
 
 func animate_player():
-	pass
+	if not is_on_floor():
+		animation.play("Jumping")
+	
+	if is_on_floor():
+		if velocity.x == 0:
+			animation.play("Idle")
+		elif velocity.x < 0 or velocity.x > 0:
+			animation.play("Walking")
+	
+	if velocity.x < 0:
+		animation.set_flip_h(true)
+	elif velocity.x > 0:
+		animation.set_flip_h(false)
